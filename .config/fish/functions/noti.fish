9563c7b8cb99bf6d79e4a85af0596b8fac29bc7f
Anmol Sethi <anmol@aubble.com>
2017-07-15 01:38:25 -0400

    .psqlrc

--- .config/fish/functions/noti.fish
+++ .config/fish/functions/noti.fish
@@ -11,7 +11,7 @@ function noti
 	if test (count $argv) -ge 3
 		set title $argv[3]
 	end
-	# TODO use proper disown command when it comes out
 	# TODO takes a lot of energy if I don't use timeout because it stays on in the background, wtf
-	fish -c "terminal-notifier -sender com.apple.Terminal -activate com.apple.Terminal -timeout 8 -message '$message' -subtitle '$subtitle' -title '$title'  ^ /dev/null &"
+	terminal-notifier -sender com.apple.Terminal -activate com.apple.Terminal -timeout 8 -message $message -subtitle $subtitle -title $title  ^ /dev/null &
+	disown (jobs -lp)
 end
--- .config/fish/functions/ymp3.fish
+++ .config/fish/functions/ymp3.fish
@@ -2,7 +2,9 @@ function ymp3 --description 'Download youtube URLs as mp3s'
 	if test (count $argv) -eq 0
 		set argv (pbpaste)
 	end
-	cd ~/Music/Downloads
-	youtube-dl --extract-audio --audio-format mp3 --default-search ytsearch $argv
+	cd (mktemp -d)
+  if youtube-dl --extract-audio --audio-format mp3 --default-search ytsearch $argv
+    mv * "$HOME/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized"
+  end
 	prevd
 end
