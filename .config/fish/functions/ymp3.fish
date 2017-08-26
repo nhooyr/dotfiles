4973148982a1b7ab1e646cfe85c3d443c53f4dd1
Anmol Sethi <anmol@aubble.com>
2017-08-26 02:43:28 -0400

    updated .config

--- .config/fish/functions/ymp3.fish
+++ .config/fish/functions/ymp3.fish
@@ -1,9 +1,9 @@
 function ymp3 --description 'Download youtube URLs as mp3s'
 	if test (count $argv) -eq 0
-		set argv (pbpaste)
+		set argv (osascript -e 'tell application "Safari" to return URL of front document')
 	end
 	cd (mktemp -d)
-  if youtube-dl --extract-audio --audio-format mp3 --default-search ytsearch $argv
+  if youtube-dl --no-playlist --extract-audio --audio-format mp3 --default-search ytsearch $argv
     mv * "$HOME/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized"
   end
 	prevd
