09be310a86495e0b8de47c81e35098c66181cc42
Anmol Sethi <me@anmol.io>
2018-08-14 08:52:44 -0400

    Updates
    Add gpg agent config, update git aliases and add noti.

--- .config/fish/functions/noti.fish
+++ .config/fish/functions/noti.fish
@@ -1,4 +1,4 @@
 function noti
 	# Activate does not actually work at the moment but whatever.
-	terminal-notifier -message "Check the terminal" -title Terminal -ignoreDnD -sender com.apple.faketerminal -activate com.apple.Terminal -sound glass
+	terminal-notifier -message "You wanted a notification" -title Terminal -ignoreDnD -sender com.apple.faketerminal -activate com.apple.Terminal -sound glass
 end
