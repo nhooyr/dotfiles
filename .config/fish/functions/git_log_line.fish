b0b1f651daf410958b63c1f32bbc35fb5de5a814
Anmol Sethi <hi@nhooyr.io>
2018-09-26 21:07:57 -0400

    More fun stuff

---
+++ .config/fish/functions/git_log_line.fish
@@ -0,0 +1,7 @@
+function git_log_line
+	if test (count $argv) != 3
+		echo "usage: "(status function)" <range_start> <range_end> <file_name>" >/dev/stderr
+		return
+	end
+	git log "-L$argv[1],$argv[2]:$argv[3]"
+end
