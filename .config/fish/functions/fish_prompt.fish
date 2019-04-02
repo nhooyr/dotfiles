915d60a08df0ad7be4855b982a38d6c928020db9
Anmol Sethi <hi@nhooyr.io>
2019-04-02 10:14:39 -0500

    Fixes

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -13,14 +13,33 @@ function fish_prompt
     set -l branch_name (branch_name)
     if test -n "$branch_name"
         echo -n :
+        set_color blue
         echo -n $branch_name
-        set -l git_status (git status --porcelain | awk '{print $1}' | uniq)
+        set_color normal
+        set_color -o
+        set -l git_status (git status --porcelain | awk '{print $1}' | sort | uniq)
         if [ (count $git_status) -gt 0 ]
             echo -n :
         end
         for s in $git_status
+            switch $s
+            case M
+                set_color green
+            case MM
+                set_color red
+            case A
+                set_color green
+            case D
+                set_color red
+            case R
+                set_color green
+            case '??'
+                set_color red
+            end
             echo -n $s
         end
+        set_color normal
+        set_color -o
     end
     
     echo
