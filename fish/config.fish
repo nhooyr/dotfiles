8713d5f76532ffa50fa1268a3e1a497fcb2be472
Anmol Sethi <hi@nhooyr.io>
2019-06-17 19:35:47 -0400

    Major fish fixes

--- fish/config.fish
+++ fish/config.fish
@@ -4,29 +4,29 @@ stty -ixon
 # Occasionally things give me errors because the default limit is so low.
 ulimit -n 16384
 
-set -g fish_greeting
-set -g fish_color_command normal
-set -g fish_color_quote green
-set -g fish_color_redirection af005f
-set -g fish_color_end af005f
-set -g fish_color_error normal
-set -g fish_color_param green
-set -g fish_color_comment 555
-# set -g fish_color_match
-# set -g fish_color_selection 
-set -g fish_color_search_match --background=bad6fe
-set -g fish_color_operator af005f
-set -g fish_color_escape af005f
+set -U fish_greeting
+set -U fish_color_command normal
+set -U fish_color_quote normal
+set -U fish_color_redirection af005f
+set -U fish_color_end af005f
+set -U fish_color_error normal
+set -U fish_color_param normal
+set -U fish_color_comment 555
+# set -U fish_color_match
+# set -U fish_color_selection
+set -U fish_color_search_match --background=bad6fe
+set -U fish_color_operator af005f
+set -U fish_color_escape af005f
 # set -g fish_color_cwd
-set -g fish_color_autosuggestion 555
+set -U fish_color_autosuggestion 555
 # set -g fish_color_user
 # set -g fish_color_host
 # set -g fish_color_cancel
 
-set -g fish_pager_color_prefix --bold black
+set -U fish_pager_color_prefix --bold black
 # set -g fish_pager_color_completion
-set -g fish_pager_color_description af005f
-set -g fish_pager_color_progress 581D5B
+set -U fish_pager_color_description af005f
+set -U fish_pager_color_progress 581D5B
 # set -g fish_pager_color_secondary
 
 # Fish expects this to be a command so we cannot directly use 'subl -wn'
@@ -62,9 +62,10 @@ set -g CDPATH . \
     ~/src/cdr \
     ~/src/codercom \
     ~/src/play \
-    ~/src
+    ~/src \
+    ~/.config
 
-set -gx FZF_DEFAULT_OPTS "--color light,bg+:153,fg+:-1,pointer:-1,prompt:-1,hl:5,hl+:5,info:-1,spinner:-1 --tabstop=4"
+set -gx FZF_DEFAULT_OPTS "--color light,bg+:153,fg+:-1,pointer:-1,prompt:-1,hl:125,hl+:125,info:-1,spinner:-1 --tabstop=4"
 
 set -l RESET_LS_COLORS 'rs=00:di=00:ln=00:mh=00:pi=00:so=00:do=00:bd=00:cd=00:or=00:mi=00:su=00:sg=00:ca=00:tw=00:ow=00:st=00:ex=00:*.tar=00:*.tgz=00:*.arc=00:*.arj=00:*.taz=00:*.lha=00:*.lz4=00:*.lzh=00:*.lzma=00:*.tlz=00:*.txz=00:*.tzo=00:*.t7z=00:*.zip=00:*.z=00:*.dz=00:*.gz=00:*.lrz=00:*.lz=00:*.lzo=00:*.xz=00:*.zst=00:*.tzst=00:*.bz2=00:*.bz=00:*.tbz=00:*.tbz2=00:*.tz=00:*.deb=00:*.rpm=00:*.jar=00:*.war=00:*.ear=00:*.sar=00:*.rar=00:*.alz=00:*.ace=00:*.zoo=00:*.cpio=00:*.7z=00:*.rz=00:*.cab=00:*.wim=00:*.swm=00:*.dwm=00:*.esd=00:*.jpg=00:*.jpeg=00:*.mjpg=00:*.mjpeg=00:*.gif=00:*.bmp=00:*.pbm=00:*.pgm=00:*.ppm=00:*.tga=00:*.xbm=00:*.xpm=00:*.tif=00:*.tiff=00:*.png=00:*.svg=00:*.svgz=00:*.mng=00:*.pcx=00:*.mov=00:*.mpg=00:*.mpeg=00:*.m2v=00:*.mkv=00:*.webm=00:*.ogm=00:*.mp4=00:*.m4v=00:*.mp4v=00:*.vob=00:*.qt=00:*.nuv=00:*.wmv=00:*.asf=00:*.rm=00:*.rmvb=00:*.flc=00:*.avi=00:*.fli=00:*.flv=00:*.gl=00:*.dl=00:*.xcf=00:*.xwd=00:*.yuv=00:*.cgm=00:*.emf=00:*.ogv=00:*.ogx=00:*.aac=00:*.au=00:*.flac=00:*.m4a=00:*.mid=00:*.midi=00:*.mka=00:*.mp3=00:*.mpc=00:*.ogg=00:*.ra=00:*.wav=00:*.oga=00:*.opus=00:*.spx=00:*.xspf=00:'
 set -l MY_LS_COLORS "di=34:ln=35:so=32:pi=32:ex=31;01"
@@ -155,15 +156,12 @@ function cdp
     end
 end
 
-bind \en 'commandline -i "; noti"'
-
 function prependSudo 
     set -l cursor (commandline -C)
     commandline -C 0
     commandline -i "sudo "
     commandline -C (math "$cursor" + 5)
 end
-bind \es prependSudo
 
 function flushdns
     sudo killall -HUP mDNSResponder
@@ -197,3 +195,16 @@ end
 function lolsay
     cowsay -f (ls  /usr/local/share/cows | cut -f 10 | gshuf | head -n 1) (fortune -o) | lolcat $argv
 end
+
+bind \en 'commandline -i "; noti"'
+bind \es prependSudo
+
+fzf_key_bindings
+function fzf-cdpath
+    set -l result (find -L $CDPATH[2..-1] -depth 1 -prune -type d -print 2> /dev/null | fzf --height 40% --query (commandline -t))
+    if [ $result ]
+        commandline -t -- "$result"
+    end
+    commandline -f repaint
+end
+bind \ec fzf-cdpath
