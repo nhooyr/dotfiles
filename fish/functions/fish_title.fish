68d0997e5886855a89fddb385c99d18666cba1f4
Anmol Sethi <hi@nhooyr.io>
2019-11-09 22:27:20 -0500

    Many small fixes

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,7 +1,8 @@
 function fish_title
     echo -n (status current-command)" "
+		echo -n (basename "$PWD")
     if [ "$hostname" != "ien" ]
-        echo -n (prompt_hostname)":"
+        echo -n "@"(prompt_hostname)
     end
-    echo (basename "$PWD")
+		echo
 end
