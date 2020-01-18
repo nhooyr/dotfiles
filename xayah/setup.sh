4ee7148231a1fa7702bd4112331c8d6d19652d1d
Anmol Sethi <hi@nhooyr.io>
2020-01-17 19:17:27 -0600

    Add gnupg pubring.kbx

--- xayah/setup.sh
+++ xayah/setup.sh
@@ -32,15 +32,15 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install -y yarn
 
-sudo ln -s "$(which /usr/bin/fdfind)" /usr/local/bin/fd > /dev/null || true 
+sudo ln -s "$(which /usr/bin/fdfind)" /usr/local/bin/fd 2> /dev/null || true
 
 sudo chsh nhooyr -s /usr/bin/fish
 
 # Install latest fzf for --info=hidden.
 mkdir -p ~/src/junegunn/fzf
 if [[ ! -d ~/src/junegunn/fzf ]]; then
-    mkdir -p ~/src/junegunn/fzf
-    git clone https://github.com/junegunn/fzf ~/src/junegunn/fzf
+  mkdir -p ~/src/junegunn/fzf
+  git clone https://github.com/junegunn/fzf ~/src/junegunn/fzf
 fi
 cd ~/src/junegunn/fzf
 GOPATH=~/.local/share/gopath /snap/bin/go install
@@ -52,3 +52,5 @@ if ! grep -q "StreamLocalBindUnlink yes" /etc/ssh/sshd_config; then
   echo "StreamLocalBindUnlink yes" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 fi
 sudo sed -i "s/#ClientAliveInterval 0/ClientAliveInterval 5/g" /etc/ssh/sshd_config
+
+gpgconf --create-socketdir
