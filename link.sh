c89b5c50e0b838fe355c135338180591db085df1
Anmol Sethi <hi@nhooyr.io>
2020-05-23 05:12:03 -0400

    Add ssh config

--- link.sh
+++ link.sh
@@ -5,6 +5,14 @@ ensure() {
   ./ci/link.sh git ~/.config/git
   ./ci/link.sh secrets/gnupg ~/.gnupg
   ./ci/link.sh secrets/ssh ~/.ssh
+  ./ci/link.sh ssh/config ~/.ssh/config
+
+  # Otherwise ssh and gpg will complain.
+  find secrets -type f -exec chmod 600 {} \;
+  find secrets -type d -exec chmod 700 {} \;
+
+  # Required for SSH multiplexing.
+  mkdir -p ~/.ssh/sockets
 }
 
 ensure_root() {
