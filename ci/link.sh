efd631d8e9c9d7f31425469bad523a7c8eb698f5
Anmol Sethi <hi@nhooyr.io>
2020-05-23 05:08:14 -0400

    Add git

--- ci/link.sh
+++ ci/link.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 main() {
-  local to
-  local src
   src="$1"
   to="$2"
 
-  if [[ -L $to ]]; then
+  if [ -L $to ]; then
     echo "overwriting $to"
     rm "$to"
-  elif [[ -e $to ]]; then
+  elif [ -e $to ]; then
     echo "$to exists and is not a symlink"
     exit 1
   else
@@ -19,7 +16,7 @@ main() {
   fi
 
   mkdir -p "$(dirname "$to")"
-  ln -s "$(realpath "$src")" "$to"
+  ln -s "$PWD/$src" "$to"
 }
 
 main "$@"
