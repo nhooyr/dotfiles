6fecf6691c61ffab7d0a21d88f7dac700c9ff65c
Anmol Sethi <hi@nhooyr.io>
2020-06-11 23:04:08 -0400

    Many fixes

--- zsh/bookmarks.zsh
+++ zsh/bookmarks.zsh
@@ -29,9 +29,3 @@ setup_bookmarks() {
   done
 }
 setup_bookmarks
-
-prepend_path CDPATH ~/src
-prepend_path CDPATH ~/.config
-prepend_path CDPATH ~/src/cdr
-prepend_path CDPATH ~/src/nhooyr
-prepend_path CDPATH ~/src/nhooyr/dotfiles
