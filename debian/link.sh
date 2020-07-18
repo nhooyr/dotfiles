a7a1c4f4d19181c15e56b969db7a309908d65bf1
Anmol Sethi <hi@nhooyr.io>
2020-07-18 09:30:28 -0400

    Remove misplaced linking scripts from the ci directory

--- debian/link.sh
+++ debian/link.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-sudo ./ci/link.sh debian/99progress-bar \
+sudo ./ln.sh debian/99progress-bar \
   /etc/apt/apt.conf.d/99progress-bar
 
-sudo ./ci/link.sh debian/green/green.sh /usr/local/bin/green.sh
-sudo ./ci/cp.sh debian/green/green.timer /etc/systemd/system/green.timer
-sudo ./ci/cp.sh debian/green/green.service /etc/systemd/system/green.service
+sudo ./ln.sh debian/green/green.sh /usr/local/bin/green.sh
+sudo ./cp.sh debian/green/green.timer /etc/systemd/system/green.timer
+sudo ./cp.sh debian/green/green.service /etc/systemd/system/green.service
 sudo systemctl enable --now green.timer
