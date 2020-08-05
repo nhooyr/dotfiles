17d2c7abb55d74bbc7ad1f4988458a53cfd3f2e7
Anmol Sethi <hi@nhooyr.io>
2020-08-05 15:57:46 -0400

    xayah: Install gcloud and kubectl

--- debian/init.sh
+++ debian/init.sh
@@ -22,7 +22,11 @@ main() {
   install_neovim
   install_docker
   install_locale
+  install_gcloud
   install_misc
+
+  # Allows searching through the package database.
+  sudo -E apt-file update
 }
 
 install_node() {
@@ -93,9 +97,10 @@ install_misc() {
     htop \
     tmux \
     apt-file \
-    apparmor-utils
-  sudo -E apt-file update
-  sudo -E aa-disable /etc/apparmor.d/usr.bin.man
+    apparmor-utils \
+    kubectl
+  # Exits with non-zero if already disabled.
+  sudo -E aa-disable /etc/apparmor.d/usr.bin.man || true
 }
 
 install_locale() {
@@ -103,6 +108,19 @@ install_locale() {
   sudo -E sed -i "s/# en_CA.UTF-8/en_CA.UTF-8/" /etc/locale.gen
   sudo -E locale-gen
   sudo -E update-locale LANG=en_CA.UTF-8
+}
+
+install_gcloud() {
+  if [ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]; then
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg]" \
+      " https://packages.cloud.google.com/apt cloud-sdk main" \
+      | sudo -E tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  fi
+  sudo -E apt install -y apt-transport-https
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    | sudo -E apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  sudo -E apt update
+  sudo -E apt install -y google-cloud-sdk
 }
 
 main "$@"
