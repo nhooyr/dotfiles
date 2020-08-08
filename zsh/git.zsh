31533500b3fe2813d9fb1e6856637f98fa541710
Anmol Sethi <hi@nhooyr.io>
2020-08-08 00:17:39 -0400

    zsh/git: Fix gch to handle existing remote origin branches

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -2,7 +2,7 @@ alias g="git"
 alias gb="git branch"
 gch() {
   if [[ "$#" -eq 1 ]]; then
-    if [ ! "$(git branch --list "$*")" ]; then
+    if [ ! "$(git branch --list "$*")" ] && [ ! "$(git branch --list --remote "origin/$*")" ]; then
       set "$*-$(xxd -p -l 2 /dev/urandom)"
       git branch "$@"
     fi
