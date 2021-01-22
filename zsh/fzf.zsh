7ef6640c64bf49700e0f40557bc4cc26a9152f4f
Anmol Sethi <hi@nhooyr.io>
2021-01-22 14:39:48 -0500

    Change statusline and fzf bg+ to white
    Prefer the neutrality so that my statusline doesn't clash with visual or
    search highlights.

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,7 +1,7 @@
 fzf_default_opts=(
   "--color dark"
-  "--color bg+:12,fg+:0,pointer:0,prompt:-1"
-  "--color hl:3,hl+:3:underline,info:-1,spinner:-1"
+  "--color bg+:7,fg+:0,pointer:0,prompt:-1"
+  "--color hl:3:underline,hl+:3:underline,info:-1,spinner:-1"
   "--tabstop=4"
   "--layout=reverse"
   "--info=hidden"
