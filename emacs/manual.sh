39687b1bf1e428ca4cbc8d267b53e8301f531956
Anmol Sethi <hi@nhooyr.io>
2023-02-20 17:03:50 -0800

    2023-02-20 05:03:50pm

--- emacs/manual.sh
+++ emacs/manual.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eu
 
-case $2 in
+case $1 in
   enc)
-    gpg --yes --armor --symmetric --cipher-algo AES256 --pinentry-mode loopback --passphrase "$1" --output manual.txt .manual.txt
+    gpg --yes --armor --symmetric --cipher-algo AES256 --pinentry-mode loopback --passphrase "$2" --output manual.txt .manual.txt
     ;;
   dec)
-    gpg --quiet --yes --armor --decrypt --pinentry-mode loopback --passphrase "$1" --output .manual.txt manual.txt
+    gpg --quiet --yes --armor --decrypt --pinentry-mode loopback --passphrase "$2" --output .manual.txt manual.txt
     ;;
   *)
     echo "second argument must be enc or dec"
