dbe04a3251d08e70552dc2db0da4161946f99897
Anmol Sethi <hi@nhooyr.io>
2023-03-27 01:21:30 -0700

    2023-03-27 01:21:30am

--- emacs/manual.sh
+++ emacs/manual.sh
@@ -4,10 +4,10 @@ cd -- "$(dirname "$0")"
 
 case $1 in
   enc)
-    gpg --yes --armor --symmetric --cipher-algo AES256 --pinentry-mode loopback --passphrase "$2" --output manual.txt .manual.txt
+    gpg --yes --armor --symmetric --cipher-algo AES256 --pinentry-mode loopback --passphrase "$2" --output - .manual.txt | base64 > manual.txt
     ;;
   dec)
-    gpg --quiet --yes --armor --decrypt --pinentry-mode loopback --passphrase "$2" --output .manual.txt manual.txt
+    base64 --decode -i manual.txt | gpg --quiet --yes --armor --decrypt --pinentry-mode loopback --passphrase "$2" --output .manual.txt -
     ;;
   *)
     echo "second argument must be enc or dec"
